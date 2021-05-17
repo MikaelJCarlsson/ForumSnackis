295afeaa531dab504704355d974119a75f27a332
MikaelJCarlsson <60873060+MikaelJCarlsson@users.noreply.github.com>
2021-05-17 14:52:46 +0200

    Inital

---
+++ Server/Data/Migrations/20210517124012_init2.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ForumSnackis.Server.Data.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ForumCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ForumCategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ForumCategories_ForumCategories_ForumCategoryId",
+                        column: x => x.ForumCategoryId,
+                        principalTable: "ForumCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubjectTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubjectDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedById = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ForumCategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subjects_AspNetUsers_CreatedById",
+                        column: x => x.CreatedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Subjects_ForumCategories_ForumCategoryId",
+                        column: x => x.ForumCategoryId,
+                        principalTable: "ForumCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PostDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PostedById = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    QuoteId = table.Column<int>(type: "int", nullable: true),
+                    SubjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_AspNetUsers_PostedById",
+                        column: x => x.PostedById,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Posts_Posts_QuoteId",
+                        column: x => x.QuoteId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Posts_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumCategories_ForumCategoryId",
+                table: "ForumCategories",
+                column: "ForumCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_PostedById",
+                table: "Posts",
+                column: "PostedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_QuoteId",
+                table: "Posts",
+                column: "QuoteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_SubjectId",
+                table: "Posts",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_CreatedById",
+                table: "Subjects",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_ForumCategoryId",
+                table: "Subjects",
+                column: "ForumCategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "ForumCategories");
+        }
+    }
+}
