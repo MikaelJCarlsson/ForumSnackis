c703c03be5a276a93e14d60ee97ea70514c147f1
MikaelJCarlsson <60873060+MikaelJCarlsson@users.noreply.github.com>
2021-05-24 17:11:55 +0200

    EventBinding in index and CategoryComponent

---
+++ Client/Pages/Index.razor.cs
@@ -0,0 +1,29 @@
+﻿using ForumSnackis.Shared.DTO;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumSnackis.Client.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public int PageState { get; set; } = 1;
+        [Parameter]
+        public CategoryDTO Category { get; set; }
+        
+        
+        [Parameter]
+        public int SubjectId { get; set; }
+        [Parameter]
+        public int CurrentPageStateChanged { get; set; }
+
+        private void UpdateCategoryState(CategoryDTO category)
+        {
+            Category = category;
+            PageState = 2;
+        }
+    }
+
+}
