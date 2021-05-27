9844b5d9375d6a47912d6517fb1a76fb2fb86c85
Ahah-it <71449901+Ahah-it@users.noreply.github.com>
2021-05-27 11:38:45 +0200

    Added postcard frontend

---
+++ Client/Shared/PostCardComponent.razor.cs
@@ -0,0 +1,19 @@
+﻿using ForumSnackis.Shared.DTO;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumSnackis.Client.Shared
+{
+    public partial class PostCardComponent : ComponentBase
+    {
+        [Parameter]
+        public PostDTO Post { get; set; }
+        protected override async Task OnParametersSetAsync()
+        {
+            this.StateHasChanged();
+        }
+    }
+}
