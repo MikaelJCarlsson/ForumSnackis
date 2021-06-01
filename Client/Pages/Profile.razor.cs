3113af72f12b3cb87bbc930ede77cf2b7a0d5372
MikaelJCarlsson <60873060+MikaelJCarlsson@users.noreply.github.com>
2021-06-01 16:53:34 +0200

    Fileupload added

--- Client/Pages/Profile.razor.cs
+++ Client/Pages/Profile.razor.cs
@@ -12,94 +12,41 @@ namespace ForumSnackis.Client.Pages
 {
     public partial class Profile : ComponentBase
     {
+		List<ImageFile> filesBase64 = new List<ImageFile>();
+		string message = "InputFile";
+		bool isDisabled = false;
 
-        private List<File> files = new();
-        private List<UploadResult> uploadResults = new();
-        private int maxAllowedFiles = 1;
-        private bool shouldRender;
+		[Inject]
+		public IHttpClientFactory HttpFactory { get; set; }
+		async Task OnChange(InputFileChangeEventArgs e)
+		{
+			var files = e.GetMultipleFiles();
+			foreach (var file in files)
+			{
+				var resizedFile = await file.RequestImageFileAsync(file.ContentType, 640, 480); // resize the image file
+				var buf = new byte[resizedFile.Size];
+				using (var stream = resizedFile.OpenReadStream())
+				{
+					await stream.ReadAsync(buf);
+				}
+				filesBase64.Add(new ImageFile { base64data = Convert.ToBase64String(buf), contentType = file.ContentType, fileName = file.Name });
+			}
+			message = "Click UPLOAD to continue";
+		}
 
-        [Inject]
-        public IHttpClientFactory HttpFactory { get; set; }
-        protected override bool ShouldRender() => shouldRender;
-
-    private async Task OnInputFileChange(InputFileChangeEventArgs e)
-    {
-        shouldRender = false;
-        long maxFileSize = 1024 * 1024 * 15;
-        var upload = false;
-
-        using var content = new MultipartFormDataContent();
-
-        foreach (var file in e.GetMultipleFiles(maxAllowedFiles))
-        {
-            if (uploadResults.SingleOrDefault(
-                f => f.FileName == file.Name) is null)
-            {
-                using var fileContent = new StreamContent(file.OpenReadStream());
-
-                files.Add(
-                    new()
-                    {
-                        Name = file.Name
-                    });
-
-                if (file.Size < maxFileSize)
-                {
-                    content.Add(
-                        content: fileContent,
-                        name: "\"files\"",
-                        fileName: file.Name);
-
-                    upload = true;
-                }
-                else
-                {
-
-                    uploadResults.Add(
-                        new()
-                        {
-                            FileName = file.Name,
-                            ErrorCode = 6,
-                            Uploaded = false
-                        });
-                }
-            }
-        }
-
-        if (upload)
-        {
-                var privateHttp = HttpFactory.CreateClient("private");
-                var publicHttp = HttpFactory.CreateClient("public");
-
-                var response = await publicHttp.PostAsync("Filesave/", content);
-                
-                var newUploadResults = await response.Content
-                    .ReadFromJsonAsync<IList<UploadResult>>();
-
-            uploadResults = uploadResults.Concat(newUploadResults).ToList();
-        }
-
-        shouldRender = true;
-    }
-
-    private static bool FileUpload(IList<UploadResult> uploadResults, string fileName, out UploadResult result)
-    {
-        result = uploadResults.SingleOrDefault(f => f.FileName == fileName);
-
-        if (result is null)
-        {
-            result = new();
-            result.ErrorCode = 5;
-        }
-
-        return result.Uploaded;
-    }
-
-    private class File
-    {
-        public string Name { get; set; }
-    }
-}
-
-
+		async Task Upload()
+		{
+			isDisabled = true;
+			var http = HttpFactory.CreateClient("private");
+			using (var msg = await http.PostAsJsonAsync<List<ImageFile>>("/api/upload", filesBase64, System.Threading.CancellationToken.None))
+			{
+				isDisabled = false;
+				if (msg.IsSuccessStatusCode)
+				{
+					message = $"{filesBase64.Count} files uploaded";
+					filesBase64.Clear();
+				}
+			}
+		}
+	}
 }
---
+++ Server/Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using ForumSnackis.Shared;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class UploadController : ControllerBase
+{
+	private readonly IWebHostEnvironment env;
+
+	public UploadController(IWebHostEnvironment env)
+	{
+		this.env = env;
+	}
+
+	[HttpPost]
+	public async Task Post([FromBody] ImageFile[] files)
+	{
+		foreach (var file in files)
+		{
+			var buf = Convert.FromBase64String(file.base64data);
+			await System.IO.File.WriteAllBytesAsync(env.ContentRootPath + "\\" + Guid.NewGuid().ToString("N") + "-" + file.fileName, buf);
+		}
+	}
+}
