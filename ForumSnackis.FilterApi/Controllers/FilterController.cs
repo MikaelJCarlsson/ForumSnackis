3fe99b336878abf145848c39b4a01e0eeb3065a0
MikaelJCarlsson <60873060+MikaelJCarlsson@users.noreply.github.com>
2021-06-02 15:43:39 +0200

    Added FilterWebApi

---
+++ ForumSnackis.FilterApi/Controllers/FilterController.cs
@@ -0,0 +1,26 @@
+﻿using ForumSnackis.FilterApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumSnackis.FilterApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilterController : ControllerBase
+    {
+        FilterService service;
+            public FilterController(FilterService service)
+            {
+            this.service = service;
+            }
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody]string content)
+        {
+          return Ok(service.CheckString(content)); 
+        }
+    }
+}
---
+++ ForumSnackis.FilterApi/Services/FilterService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumSnackis.FilterApi.Services
+{
+    public class FilterService
+    {
+        private HashSet<string> Bannedwords;
+
+        public FilterService()
+        {
+            Bannedwords = new();
+            Bannedwords.Add("kuk");
+        }
+
+        public string CheckString(string content)
+        {
+            char [] delims = { ' ', '.', ';' };
+            var toCheck = content.ToLower().Split(delims);
+            string result = content;
+            foreach (var c in toCheck)
+            {
+                if (Bannedwords.Contains(c))
+                {
+                    result = result.Replace(c, "*****",StringComparison.OrdinalIgnoreCase);
+                }               
+            }
+            return result;
+        }
+    }
+}
