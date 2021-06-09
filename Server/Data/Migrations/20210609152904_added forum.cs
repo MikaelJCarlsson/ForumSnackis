85cbb23d01ca90d0004182052cc7a3faecb3edd8
Ahah-it <Ahah-it@users.noreply.github.com>
2021-06-09 18:02:18 +0200

    Migrations

---
+++ Server/Data/Migrations/20210609152904_added forum.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ForumSnackis.Server.Data.Migrations
+{
+    public partial class addedforum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subjects_ForumCategories_ForumCategoryId",
+                table: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "ForumCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_ForumCategoryId",
+                table: "Subjects");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Subjects",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoriesId",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Forums",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Forums", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_CategoryId",
+                table: "Subjects",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoriesId",
+                table: "Categories",
+                column: "CategoriesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Forums_CategoriesId",
+                table: "Categories",
+                column: "CategoriesId",
+                principalTable: "Forums",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subjects_Categories_CategoryId",
+                table: "Subjects",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Forums_CategoriesId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subjects_Categories_CategoryId",
+                table: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "Forums");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_CategoryId",
+                table: "Subjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_CategoriesId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Subjects");
+
+            migrationBuilder.DropColumn(
+                name: "CategoriesId",
+                table: "Categories");
+
+            migrationBuilder.CreateTable(
+                name: "ForumCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoriesId = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ForumCategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ForumCategories_Categories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_ForumCategoryId",
+                table: "Subjects",
+                column: "ForumCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumCategories_CategoriesId",
+                table: "ForumCategories",
+                column: "CategoriesId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subjects_ForumCategories_ForumCategoryId",
+                table: "Subjects",
+                column: "ForumCategoryId",
+                principalTable: "ForumCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
