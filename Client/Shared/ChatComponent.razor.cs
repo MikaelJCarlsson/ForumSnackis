3fa3d14eb76b846a07074caef3ed8b73a326d3b2
Ahah-it <Ahah-it@users.noreply.github.com>
2021-06-10 13:12:21 +0200

    UI

---
+++ Client/Shared/ChatComponent.razor.cs
@@ -0,0 +1,24 @@
+﻿using ForumSnackis.Shared.DTO;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace ForumSnackis.Client.Shared
+{
+    public partial class ChatComponent : ComponentBase
+    {
+        public class ChatMessage {
+
+            public string Message { get; set; } = "";
+        }
+
+        public ChatMessage MessageModel { get; set; } = new();
+        private void subas() {
+
+        }
+    }
+}
