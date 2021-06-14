2e1713fa5393f22a54dc455d588fb6d3b2bdd614
MikaelJCarlsson <60873060+MikaelJCarlsson@users.noreply.github.com>
2021-06-14 14:32:15 +0200

    ImageInput now displays image name

--- Client/Pages/Posts.razor.cs
+++ Client/Pages/Posts.razor.cs
@@ -3,6 +3,7 @@ using ForumSnackis.Shared;
 using ForumSnackis.Shared.DTO;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,8 @@ namespace ForumSnackis.Client.Pages
 {
     public partial class Posts : ComponentBase
     {
+        List<ImageFile> filesBase64 = new List<ImageFile>();
+        bool isDisabled = false;
         [Parameter]
         public int SubjectId { get; set; }
         [Parameter]
@@ -89,16 +92,54 @@ namespace ForumSnackis.Client.Pages
 
         public async Task CreatePost()
         {
-            if (privateHttp is null && await CreatePrivateHttpClientAsync() && NewPost?.Content?.Length > 2)
+            if (privateHttp is null && await CreatePrivateHttpClientAsync() && NewPost?.Content?.Length > 2 || filesBase64 != null)
             {
                 NewPost.SubjectId = SubjectId;
-
+                if(filesBase64 != null && string.IsNullOrEmpty(NewPost.Content))
+                {
+                    NewPost.Content = "";
+                }
                 var request = await privateHttp.PostAsJsonAsync("api/Subject/Posts/", NewPost);
 
                 if (request.IsSuccessStatusCode)
                 {
                     NewPost.Content = "";
+                    if(filesBase64 != null)
+                    {
+                        var id = await request.Content.ReadFromJsonAsync<int>();
+                        await Upload(id);
+                    }
                     await UpdatePosts();
+                    
+                }
+            }
+        }
+        async Task OnChange(InputFileChangeEventArgs e)
+        {
+            var files = e.GetMultipleFiles();
+            foreach (var file in files)
+            {
+                var resizedFile = await file.RequestImageFileAsync(file.ContentType, 128, 128); // resize the image file
+                var buf = new byte[resizedFile.Size];
+                using (var stream = resizedFile.OpenReadStream())
+                {
+                    await stream.ReadAsync(buf);
+                }
+                filesBase64.Add(new ImageFile { base64data = Convert.ToBase64String(buf), contentType = file.ContentType, fileName = file.Name });
+            }
+
+        }
+
+        async Task Upload(int id)
+        {
+            isDisabled = true;
+            var http = HttpFactory.CreateClient("private");
+            using (var msg = await http.PostAsJsonAsync($"/api/upload/{id}", filesBase64, System.Threading.CancellationToken.None))
+            {
+                isDisabled = false;
+                if (msg.IsSuccessStatusCode)
+                {
+                    filesBase64.Clear();
                 }
             }
         }
